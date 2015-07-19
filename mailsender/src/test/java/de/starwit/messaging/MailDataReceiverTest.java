86c97017297fa3fb4395d04bd50b35e9385d974e
ztarbug <kronprinzfriedrich@gmx.de>
2015-07-19 23:20:39 +0200

    initial checkin
    * web project with mdbs for JMS messaging
    * jar lib with DTOs

---
+++ mailsender/src/test/java/de/starwit/messaging/MailDataReceiverTest.java
@@ -0,0 +1,11 @@
+package de.starwit.messaging;
+
+import junit.framework.TestCase;
+
+public class MailDataReceiverTest extends TestCase {
+
+	public void testOnMessage() {
+		//fail("Not yet implemented");
+	}
+
+}
